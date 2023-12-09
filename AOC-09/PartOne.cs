cbca722568f279ce4f80bba745e3d8a344cc7186
Tomasz Zakręcki <tzakrecki@gmail.com>
2023-12-09 11:51:15 +0100

    Day-9

---
+++ AOC-09/PartOne.cs
@@ -0,0 +1,45 @@
+﻿namespace AOC_01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class PartOne
+    {
+        public long Handle()
+        {
+            long num = 0;
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+                num += this.CalculateLine(line);
+            }
+
+            return num;
+        }
+
+        private long CalculateLine(string line)
+        {
+            var items = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+            var lastItems = new List<long>();
+            while (!items.All(x => x == 0))
+            {
+                lastItems.Add(items.LastOrDefault());
+
+                var list = new List<long>();
+                for (int i = 0; i < items.Count - 1; i++)
+                {
+                    list.Add(items[i + 1] - items[i]);
+                }
+
+                items = list;
+            }
+            return lastItems.Sum();
+
+        }
+    }
+}
---
+++ AOC-09/PartTwo.cs
@@ -0,0 +1,49 @@
+﻿namespace AOC_01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class PartTwo
+    {
+        public long Handle()
+        {
+            long num = 0;
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+                num += this.CalculateLine(line);
+            }
+
+            return num;
+        }
+
+        private long CalculateLine(string line)
+        {
+            var items = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+            var lastItems = new List<long>();
+            while (!items.All(x => x == 0))
+            {
+                lastItems.Add(items.FirstOrDefault());
+
+                var list = new List<long>();
+                for (int i = 0; i < items.Count - 1; i++)
+                {
+                    list.Add(items[i + 1] - items[i]);
+                }
+
+                items = list;
+            }
+            long num = lastItems[lastItems.Count - 1];
+            for (int i = lastItems.Count - 2; i >= 0; i--)
+            {
+                num = lastItems[i] - num;
+            }
+            return num;
+        }
+    }
+}
